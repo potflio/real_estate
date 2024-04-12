eddec0cac56ce0756c5662c04d05481474a1a720
PrakashPandian <120122316+potflio@users.noreply.github.com>
2024-04-12 18:28:58 +0530

    Update settings.py

--- real_estate_project/real_estate_project/settings.py
+++ real_estate_project/real_estate_project/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'real_estate',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'real_estate',
         'HOST': 'localhost',
         'PORT': '3306', 
     }
